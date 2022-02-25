febcf450bb01d749300142f3314aa650c4807acc
chairat_rog <chaidev.cm@gmail.com>
2022-02-25 18:06:51 +0700

    Test commit on VSCode

---
+++ Go/hellogo/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type yoyo struct {
+
+}
+
+type xy int
+
+func main() {
+	xi := []int{1, 2, 4, 5, 632, 346346}
+	fmt.Println(xi)
+
+	m := map[string]int{
+		"T1": 435,
+		"T2": 42,
+	}
+	fmt.Println(m)
+}
